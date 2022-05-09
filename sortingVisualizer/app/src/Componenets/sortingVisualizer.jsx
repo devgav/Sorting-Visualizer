e8ac226947eb1a2e304efbbe42c8f074ba2d43d8
Gavin Peng <blackbullet54321@gmail.com>
2022-05-08 22:43:16 -1000

    Implement visualizer for sort

--- sortingVisualizer/app/src/Componenets/sortingVisualizer.jsx
+++ sortingVisualizer/app/src/Componenets/sortingVisualizer.jsx
@@ -2,10 +2,15 @@ import React, { useEffect, useState } from "react";
 import "./sortingVisualizer.css";
 import Navbar from "./Navbar/Navbar";
 import { BubbleSort } from "../algorithms/BubbleSort";
+import { InsertionSort } from "../algorithms/InsertionSort";
 
 
 const SortVisualizer = () => {
     const [rectangle, setRectangle] = useState([]);
+    const [isSorting, setSorting] = useState(false);
+    const [isSorted, setSorted] = useState(false);
+    const containerRef = null;
+    
 
     function randomNumber(max, min) {
         return Math.floor(Math.random() * (max - min) + min);
@@ -13,40 +18,94 @@ const SortVisualizer = () => {
 
     function generateRectangle() {
         let rectangleArr = [];
-        let barStyling = document.getElementsByClassName('rectangularity');
-        console.log(barStyling);
+        let barStyling = document.getElementsByClassName("rectangularity");
         for (let i = 0; i <= 78; i++) {
+            if (barStyling[i] != null) { 
+                barStyling[i].style.backgroundColor = "lightgreen";
+            }
             rectangleArr.push(randomNumber(800, 10));
         }
         setRectangle(rectangleArr);
     }
-
-    function bubbleSort() {
-        console.log("entered bubblesort")
-        const animations = BubbleSort(rectangle);
-        const barStyling = document.querySelectorAll('.rectangularity');
-        barStyling[1].style.backgroundColor = "red";
-        barStyling[2].style.backgroundColor = "black";
-        let bar1;
-        let bar2;
-        for (let i = 0; i < animations.length; ++i) {
-            bar1 = animations[i].i;
-            bar2 = animations[i].j;
-            console.log(barStyling[bar1].style.backgroundColor = "orange");
-            console.log(barStyling[bar2]);
+    
+    function accessArray(index) {
+        //return the array of bars generated in our div
+        const bars = containerRef.current.children;
+        //get the color of our bar
+        const barStyling = bars[index].style;
+        //Changes to red when sorting
+        setTimeout(() => { 
+            //sorted color
+            barStyling.backgroundColor = "red";
+        }, 5);
+        //changes back to original color after sorted 
+        setTimeout(() => {
+            //sorted color
+            barStyling.backgroundColor = "";
+        }, 5 * 2);
+    }
+    
+    function animateArray() {
+        //return the array of bars generated in our div
+        const bars = containerRef.current.children;
+        for (let i = 0; i <= 78; i++) { 
+            const barStyle = bars[i].style;
+            setTimeout(() => (barStyle.backgroundColor = "red"), i * 10);
         }
-        // setTimeout(() => {
-        //     barStyling[bar1].style.backgroundColor = "green";
-        //     barStyling[bar2].style.backgroundColor = "green";
-        // }, 2 * 2);
+        setTimeout(() => { 
+            setSorted(true);
+            setSorting(false);
+        }, bars.length * 10);
     }
 
-    useEffect(() => {
-        generateRectangle();
-    }, []);
+    function bubbleSort() {
+        const animations = InsertionSort(rectangle);
+        if (isSorting) { 
+            return;
+        }
+        setSorting(true);
+        animations.forEach(([comparison, swapped], i) => { 
+            setTimeout(() => {
+                // no swap before and we have two elements [[indexToSwap, heightOfRectangle], swapped]
+                if (!swapped) {
+                    if (comparison.length === 2) {
+                        const [i, j] = comparison;
+                        accessArray(i);
+                        accessArray(j);
+                    } else { 
+                        //set the [i] to the array which contains [indexToSwap, heightOfRectangle]
+                        const [i] = comparison; 
+                        accessArray(i);
+                    }
+                } else { 
+                    setRectangle((prevRectangle) => { 
+                        const [k, newValue] = comparison;
+                        //make a copy of the previous rectangles
+                        const newArr = [...prevRectangle];
+                        newArr[k] = newValue;
+                        return newArr;
+                    });
+                }
+            }, (i * 10));
+        });
+        setTimeout(() => { 
+            animateArray();
+        }, 78 * 10);
+        // for (let i = 0; i < animations.length; i++) {
+        //     setTimeout(() => {
+        //         bar1 = animations[i];
+        //         bar2 = animations[i];
+        //         barStyling[bar1].style.backgroundColor = "red";
+        //         barStyling[bar2].style.backgroundColor = "red";
+        //        
+        //     }, i * 10);
+        // }
+    }
+
+    useEffect(generateRectangle, []);
 
     return (
-        <div>
+        <div ref={containerRef}>
             <Navbar
                 // onMergeSort={mergeSort}
                 // onMergeSort={mergeSort}
@@ -57,7 +116,7 @@ const SortVisualizer = () => {
                 <div
                     className="rectangularity"
                     key={index}
-                    style={{ height: `${value}px` }}>
+                    style={{ height: `${value}px`, width: `${100/78}vw` }}>
                 </div>
             ))}
         </div>
--- sortingVisualizer/app/src/algorithms/BubbleSort.js
+++ sortingVisualizer/app/src/algorithms/BubbleSort.js
@@ -1,18 +1,15 @@
+import { swap } from "./Swap";
+
 export function BubbleSort(rectangleArr) { 
     let animations = [];
     for(let i = 0; i < rectangleArr.length; i++) { 
         for(let j=0; j < rectangleArr.length-i-1; j++) { 
             if(rectangleArr[j] > rectangleArr[j + 1]) { 
-                animations.push({i, j});
+                animations.push([[i, j], true]);
+                animations.push([[i, j], true]);
                 swap(i, j, rectangleArr);
             }
         }
-    }
-    
-    function swap(i, j, arr) {
-        let temp = arr[j];
-        arr[j] = arr[j + 1];
-        arr[j + 1] = temp;
     }
     return animations;
 }
